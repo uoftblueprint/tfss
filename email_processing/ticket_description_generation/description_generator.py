c190cc7e230c74296f01e0a62f2b5956d3708c71
Pierre-William Lessard <lessardpw@gmail.com>
2023-10-29 15:20:56 -0400

    file structure for email processing description generation

---
+++ email_processing/ticket_description_generation/description_generator.py
@@ -0,0 +1,17 @@
+from config import API_KEY
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class Email:
+    to: List[str]
+    author: str
+    date: str
+    subject: str
+    body: str
+
+# TODO: create a function to prompt chatGPT given some input
+
+
+# TODO: create a function that when inputted some email object, 
+# prompts chatGPT and returns the description of a task defined from the email
